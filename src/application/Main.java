cb71f855003177bbfb4be8442e8c9d592520747c
Roger Augusto <rogeraugustosoares@gmail.com>
2019-11-17 16:04:10 -0300

    About View

--- src/application/Main.java
+++ src/application/Main.java
@@ -10,6 +10,9 @@ import javafx.scene.control.ScrollPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
+	
+	private static Scene mainScene;
+	
 	@Override
 	public void start(Stage primaryStage) {
 		try {
@@ -19,14 +22,19 @@ public class Main extends Application {
 			scrollPane.setFitToHeight(true);
 			scrollPane.setFitToWidth(true);
 			
-			Scene mainScene = new Scene(scrollPane);
+			mainScene = new Scene(scrollPane);
 			primaryStage.setScene(mainScene);
-			primaryStage.setTitle("Aplicação JavaFX");
+			primaryStage.setTitle("Aplicação Workshop");
 			primaryStage.show();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
+	
+	public static Scene getMainScene() {
+		return mainScene;
+	}
+
 
 	public static void main(String[] args) {
 		launch(args);
